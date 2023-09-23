57301af0bad9d98ec7cd6f04ef555a8a6f6b9c20
ya-ilya <ilya.tseplaev@gmail.com>
2023-09-24 00:35:35 +0300

    Fixed MixinGuiScreen

--- src/main/java/com/kisman/cc/mixin/mixins/MixinGuiScreen.java
+++ src/main/java/com/kisman/cc/mixin/mixins/MixinGuiScreen.java
@@ -9,20 +9,21 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiYesNoCallback;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiScreen.class)
 public class MixinGuiScreen extends Gui implements GuiYesNoCallback {
-    @Shadow public Minecraft mc;
-
     @Override
     public void confirmClicked(boolean b, int i) {}
 
     @Inject(method = "drawDefaultBackground", at = @At("HEAD"), cancellable = true)
-    public void drawDefaultBackgroundHook(CallbackInfo ci) {if (NoRender.instance.guiOverlay.getValBoolean() && mc.world != null && mc.player != null) ci.cancel();}
+    public void drawDefaultBackgroundHook(CallbackInfo ci) {
+        if (NoRender.instance.guiOverlay.getValBoolean() && Minecraft.getMinecraft().world != null) {
+            ci.cancel();
+        }
+    }
 
     @Inject(method = "renderToolTip", at = @At("HEAD"), cancellable = true)
     private void toolTipHook(ItemStack itemStack, int x, int y, CallbackInfo ci) {
